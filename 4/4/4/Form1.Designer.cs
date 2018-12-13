486eef05a4805f198e7ebdf5e5b28ab5b35eaa25
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-12-13 20:10:58 +0100

    Add files via upload

---
+++ 4/4/4/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace _4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbAntwoord1 = new System.Windows.Forms.TextBox();
+            this.lblAntwoord1 = new System.Windows.Forms.Label();
+            this.lblAntwoord2 = new System.Windows.Forms.Label();
+            this.tbAntwoord2 = new System.Windows.Forms.TextBox();
+            this.tbAntwoord3 = new System.Windows.Forms.TextBox();
+            this.tbAntwoord4 = new System.Windows.Forms.TextBox();
+            this.lblAntwoord3 = new System.Windows.Forms.Label();
+            this.lblAntwoord4 = new System.Windows.Forms.Label();
+            this.btnAntwoorden = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbAntwoord1
+            // 
+            this.tbAntwoord1.Location = new System.Drawing.Point(79, 41);
+            this.tbAntwoord1.Name = "tbAntwoord1";
+            this.tbAntwoord1.Size = new System.Drawing.Size(100, 20);
+            this.tbAntwoord1.TabIndex = 0;
+            // 
+            // lblAntwoord1
+            // 
+            this.lblAntwoord1.AutoSize = true;
+            this.lblAntwoord1.Location = new System.Drawing.Point(12, 44);
+            this.lblAntwoord1.Name = "lblAntwoord1";
+            this.lblAntwoord1.Size = new System.Drawing.Size(61, 13);
+            this.lblAntwoord1.TabIndex = 1;
+            this.lblAntwoord1.Text = "Antwoord 1";
+            // 
+            // lblAntwoord2
+            // 
+            this.lblAntwoord2.AutoSize = true;
+            this.lblAntwoord2.Location = new System.Drawing.Point(12, 70);
+            this.lblAntwoord2.Name = "lblAntwoord2";
+            this.lblAntwoord2.Size = new System.Drawing.Size(61, 13);
+            this.lblAntwoord2.TabIndex = 2;
+            this.lblAntwoord2.Text = "Antwoord 2";
+            // 
+            // tbAntwoord2
+            // 
+            this.tbAntwoord2.Location = new System.Drawing.Point(79, 67);
+            this.tbAntwoord2.Name = "tbAntwoord2";
+            this.tbAntwoord2.Size = new System.Drawing.Size(100, 20);
+            this.tbAntwoord2.TabIndex = 3;
+            // 
+            // tbAntwoord3
+            // 
+            this.tbAntwoord3.Location = new System.Drawing.Point(79, 93);
+            this.tbAntwoord3.Name = "tbAntwoord3";
+            this.tbAntwoord3.Size = new System.Drawing.Size(100, 20);
+            this.tbAntwoord3.TabIndex = 4;
+            // 
+            // tbAntwoord4
+            // 
+            this.tbAntwoord4.Location = new System.Drawing.Point(79, 119);
+            this.tbAntwoord4.Name = "tbAntwoord4";
+            this.tbAntwoord4.Size = new System.Drawing.Size(100, 20);
+            this.tbAntwoord4.TabIndex = 5;
+            // 
+            // lblAntwoord3
+            // 
+            this.lblAntwoord3.AutoSize = true;
+            this.lblAntwoord3.Location = new System.Drawing.Point(12, 96);
+            this.lblAntwoord3.Name = "lblAntwoord3";
+            this.lblAntwoord3.Size = new System.Drawing.Size(61, 13);
+            this.lblAntwoord3.TabIndex = 6;
+            this.lblAntwoord3.Text = "Antwoord 3";
+            // 
+            // lblAntwoord4
+            // 
+            this.lblAntwoord4.AutoSize = true;
+            this.lblAntwoord4.Location = new System.Drawing.Point(12, 122);
+            this.lblAntwoord4.Name = "lblAntwoord4";
+            this.lblAntwoord4.Size = new System.Drawing.Size(61, 13);
+            this.lblAntwoord4.TabIndex = 7;
+            this.lblAntwoord4.Text = "Antwoord 4";
+            // 
+            // btnAntwoorden
+            // 
+            this.btnAntwoorden.Location = new System.Drawing.Point(79, 12);
+            this.btnAntwoorden.Name = "btnAntwoorden";
+            this.btnAntwoorden.Size = new System.Drawing.Size(75, 23);
+            this.btnAntwoorden.TabIndex = 8;
+            this.btnAntwoorden.Text = "Antwoorden";
+            this.btnAntwoorden.UseVisualStyleBackColor = true;
+            this.btnAntwoorden.Click += new System.EventHandler(this.btnAntwoorden_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 201);
+            this.Controls.Add(this.btnAntwoorden);
+            this.Controls.Add(this.lblAntwoord4);
+            this.Controls.Add(this.lblAntwoord3);
+            this.Controls.Add(this.tbAntwoord4);
+            this.Controls.Add(this.tbAntwoord3);
+            this.Controls.Add(this.tbAntwoord2);
+            this.Controls.Add(this.lblAntwoord2);
+            this.Controls.Add(this.lblAntwoord1);
+            this.Controls.Add(this.tbAntwoord1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbAntwoord1;
+        private System.Windows.Forms.Label lblAntwoord1;
+        private System.Windows.Forms.Label lblAntwoord2;
+        private System.Windows.Forms.TextBox tbAntwoord2;
+        private System.Windows.Forms.TextBox tbAntwoord3;
+        private System.Windows.Forms.TextBox tbAntwoord4;
+        private System.Windows.Forms.Label lblAntwoord3;
+        private System.Windows.Forms.Label lblAntwoord4;
+        private System.Windows.Forms.Button btnAntwoorden;
+    }
+}
+
---
+++ 4/4/4/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAntwoorden_Click(object sender, EventArgs e)
+        {
+            tbAntwoord1.Text = Convert.ToString(-1 + 4 * 6);
+            tbAntwoord2.Text = Convert.ToString((35 + 5) % 7);
+            tbAntwoord3.Text = Convert.ToString(14 + -4 * 6 / 11);
+            tbAntwoord4.Text = Convert.ToString(2 + 15 / 6 * 1 - 7 % 2);
+        }
+    }
+}
