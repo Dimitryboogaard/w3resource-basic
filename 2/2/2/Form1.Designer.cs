8f3c6a2642d1d131617a58b9179743a3bff50e7e
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-12-13 20:09:30 +0100

    Add files via upload

---
+++ 2/2/2/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace _2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbGetal1 = new System.Windows.Forms.TextBox();
+            this.tbGetal2 = new System.Windows.Forms.TextBox();
+            this.lblGetal1 = new System.Windows.Forms.Label();
+            this.lblGetal2 = new System.Windows.Forms.Label();
+            this.btnSom = new System.Windows.Forms.Button();
+            this.tbSom = new System.Windows.Forms.TextBox();
+            this.lblSom = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbGetal1
+            // 
+            this.tbGetal1.Location = new System.Drawing.Point(59, 12);
+            this.tbGetal1.Name = "tbGetal1";
+            this.tbGetal1.Size = new System.Drawing.Size(100, 20);
+            this.tbGetal1.TabIndex = 0;
+            // 
+            // tbGetal2
+            // 
+            this.tbGetal2.Location = new System.Drawing.Point(59, 41);
+            this.tbGetal2.Name = "tbGetal2";
+            this.tbGetal2.Size = new System.Drawing.Size(100, 20);
+            this.tbGetal2.TabIndex = 1;
+            // 
+            // lblGetal1
+            // 
+            this.lblGetal1.AutoSize = true;
+            this.lblGetal1.Location = new System.Drawing.Point(12, 15);
+            this.lblGetal1.Name = "lblGetal1";
+            this.lblGetal1.Size = new System.Drawing.Size(41, 13);
+            this.lblGetal1.TabIndex = 2;
+            this.lblGetal1.Text = "Getal 1";
+            // 
+            // lblGetal2
+            // 
+            this.lblGetal2.AutoSize = true;
+            this.lblGetal2.Location = new System.Drawing.Point(12, 44);
+            this.lblGetal2.Name = "lblGetal2";
+            this.lblGetal2.Size = new System.Drawing.Size(41, 13);
+            this.lblGetal2.TabIndex = 3;
+            this.lblGetal2.Text = "Getal 2";
+            // 
+            // btnSom
+            // 
+            this.btnSom.Location = new System.Drawing.Point(59, 67);
+            this.btnSom.Name = "btnSom";
+            this.btnSom.Size = new System.Drawing.Size(75, 23);
+            this.btnSom.TabIndex = 4;
+            this.btnSom.Text = "Som";
+            this.btnSom.UseVisualStyleBackColor = true;
+            this.btnSom.Click += new System.EventHandler(this.btnSom_Click);
+            // 
+            // tbSom
+            // 
+            this.tbSom.Location = new System.Drawing.Point(59, 96);
+            this.tbSom.Name = "tbSom";
+            this.tbSom.Size = new System.Drawing.Size(100, 20);
+            this.tbSom.TabIndex = 5;
+            // 
+            // lblSom
+            // 
+            this.lblSom.AutoSize = true;
+            this.lblSom.Location = new System.Drawing.Point(25, 99);
+            this.lblSom.Name = "lblSom";
+            this.lblSom.Size = new System.Drawing.Size(28, 13);
+            this.lblSom.TabIndex = 6;
+            this.lblSom.Text = "Som";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(213, 147);
+            this.Controls.Add(this.lblSom);
+            this.Controls.Add(this.tbSom);
+            this.Controls.Add(this.btnSom);
+            this.Controls.Add(this.lblGetal2);
+            this.Controls.Add(this.lblGetal1);
+            this.Controls.Add(this.tbGetal2);
+            this.Controls.Add(this.tbGetal1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbGetal1;
+        private System.Windows.Forms.TextBox tbGetal2;
+        private System.Windows.Forms.Label lblGetal1;
+        private System.Windows.Forms.Label lblGetal2;
+        private System.Windows.Forms.Button btnSom;
+        private System.Windows.Forms.TextBox tbSom;
+        private System.Windows.Forms.Label lblSom;
+    }
+}
+
---
+++ 2/2/2/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSom_Click(object sender, EventArgs e)
+        {
+            if(btnSom.Text == "Som")
+            {
+                btnSom.Text = "Opnieuw";
+                tbSom.Text = Convert.ToString(Convert.ToInt16(tbGetal1.Text) + Convert.ToInt16(tbGetal2.Text));
+            }
+
+            else if(btnSom.Text == "Opnieuw")
+            {
+                btnSom.Text = "Som";
+                tbGetal1.Text = "";
+                tbGetal2.Text = "";
+                tbSom.Text = "";
+            }
+        }
+    }
+}
